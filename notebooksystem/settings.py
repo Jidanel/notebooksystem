401add0bed778799721e6643341005dabc86ed32
Jidanel <you@example.com>
2024-08-10 20:04:27 +0100

    settings

--- notebooksystem/settings.py
+++ notebooksystem/settings.py
@@ -133,6 +133,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
